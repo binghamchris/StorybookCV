da04f31bbe1eb71bf4a5cee9691448eac85125f4
Chris Bingham <chris@pearit.co.uk>
2015-05-24 15:35:23 +0200

    HTML Structure Complete
    Completed first version of HTML structure and related image and JS
    resources.

---
+++ js/skillschart.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+    var skillsData = {
+        labels: ['Enterprise Linux', 'OpenStack', 'Amazon Web Services', 'Computer Systems Validation (GxP)', 'Team Leading', 'Gluster', 'Puppet', 'Perl', 'Technical Training', 'Customer Requirements Gathering'],
+        series: [25, 20, 10, 10, 10, 5, 5, 5, 5, 5]
+    };
+	
+	var options = {
+        donut: true
+	};
+				
+	var responsiveOptions = [
+        ['screen and (min-width: 640px)', {
+            chartPadding: 5,
+            labelOffset: 50,
+            labelDirection: 'explode',
+            donutWidth: 80
+        }],
+        ['screen and (min-width: 768px)', {
+            chartPadding: 20,
+            labelOffset: 80,
+            labelDirection: 'explode',
+            donutWidth: 100
+        }],
+        ['screen and (min-width: 1200px)', {
+            chartPadding: 30,
+            labelOffset: 100,
+            labelDirection: 'explode',
+            donutWidth: 120
+        }]
+    ];
+				
+    new Chartist.Pie('#skillsChart', skillsData, options, responsiveOptions);
+};	
