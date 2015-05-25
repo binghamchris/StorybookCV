64de646fdf826ef84a9cfdc3cd914834f1d29c8d
Chris Bingham <chris@pearit.co.uk>
2015-05-25 15:24:58 +0200

    New themes added
    Added 2 additional themes; "Shuttle" and "Tardis".
    Added JS code to randomly rotate between themes.
    HTML checked for compliance with HTML5 using W3C markup validation
    service.

--- js/storybook.js
+++ js/storybook.js
@@ -1,4 +1,11 @@
 window.onload = function(){
+    //Select a theme at random
+    var arrThemes = ['css/colours-thunderbird.css','css/colours-tardis.css','css/colours-shuttle.css']
+    var selectedCss = arrThemes[Math.floor(Math.random() * arrThemes.length)]
+    
+    document.getElementById('colours-css').href = selectedCss
+    
+    //Generate the skills donut chart
     var skillsData = {
         labels: ['Enterprise Linux', 'OpenStack', 'Amazon Web Services', 'Computer Systems Validation (GxP)', 'Team Leading', 'Gluster', 'Puppet', 'Perl', 'Technical Training', 'Customer Requirements Gathering'],
         series: [25, 20, 10, 10, 10, 5, 5, 5, 5, 5]
