5df64ee5d2a5ac8f2401ce0a717ab70ad6e7756a
Chris Bingham <chris@pearit.co.uk>
2015-06-02 10:29:05 +0200

    Fixes for Issues #3, #5, and #6
    Issue #3: Added media query to adjust the position of the background
    image and changed the layout to a 2 row format for the jumbotron.
    Issue #5: Adjusted JS for chart generation to modify label placement,
    modified content of the CSV label, and altered CSS to set a min and max
    height for the chart area on all screen sizes.
    Issue #6: Removed extra '>' character.

--- js/storybook.js
+++ js/storybook.js
@@ -1,13 +1,15 @@
 window.onload = function(){
     //Select a theme at random
     var arrThemes = ['css/colours-thunderbird.css','css/colours-tardis.css','css/colours-shuttle.css']
+
+    //var arrThemes = ['css/colours-shuttle.css']
     var selectedCss = arrThemes[Math.floor(Math.random() * arrThemes.length)]
     
     document.getElementById('colours-css').href = selectedCss
     
     //Generate the skills donut chart
     var skillsData = {
-        labels: ['Enterprise Linux', 'OpenStack', 'Amazon Web Services', 'Computer Systems Validation (GxP)', 'Team Leading', 'Gluster', 'Puppet', 'Perl', 'Technical Training', 'Customer Requirements Gathering'],
+        labels: ['Enterprise Linux', 'OpenStack', 'Amazon Web Services', 'Systems Validation (GxP)', 'Team Leading', 'Gluster', 'Puppet', 'Perl', 'Technical Training', 'Customer Requirements Gathering'],
         series: [25, 20, 10, 10, 10, 5, 5, 5, 5, 5]
     };
 	
@@ -16,23 +18,19 @@ window.onload = function(){
 	};
 				
 	var responsiveOptions = [
-        ['screen and (min-width: 640px)', {
-            chartPadding: 5,
+        ['screen and (max-width: 767px)', {
+            chartPadding: 10,
             labelOffset: 50,
             labelDirection: 'explode',
-            donutWidth: 80
+            donutWidth: 60,
+            chartPadding: 60
         }],
-        ['screen and (min-width: 768px)', {
+        ['screen and (min-width: 767px)', {
             chartPadding: 20,
-            labelOffset: 80,
+            labelOffset: 70,
             labelDirection: 'explode',
-            donutWidth: 100
-        }],
-        ['screen and (min-width: 1200px)', {
-            chartPadding: 35,
-            labelOffset: 80,
-            labelDirection: 'explode',
-            donutWidth: 110
+            donutWidth: 90,
+            chartPadding: 50
         }]
     ];
 				
